9897f714b976b8f0d833c5821a501b9e1c17a02f
= <=>
2018-06-14 17:39:37 +0700

    PSD

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -33,16 +33,11 @@
 // 		window.setTimeout("slideShow("+imageNumber+")", 2000);
 // 	}
 // }
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+function myFunction() {
+    var x = document.getElementById("categoriea");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
